cd5819125e71d3db37bda4d9873c74eae73299a2
Carlos A <cayala.w@gmail.com>
2025-10-22 23:25:29 -0500

    Fright night coding: Day 1/Step 1

---
+++ fright-night-coding/find-code/src/main/java/com/gerbosan/App.java
@@ -0,0 +1,104 @@
+package com.gerbosan;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.Collection;
+import java.util.Collections;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    // String filePath = "hyperskill-dataset-117267365.txt";
+    // String data = ReadFile.returnContent(filePath);
+    String data = "2904612814265124950186150241288486039555324630611109544186388550429967794529563707050650703320339036303462335384680001635099845625207021105726257804565606103838537290638121317541338115759138705042919783275742443076834276284559804555328509747993798405812273351973853800426683080375361218215688342894425548144853863719765915342592038704966737115331740496229192849607808619449322065100883244874724693566475782324898011670251914418418718579383005330365012983996394588625731220697740582918132951070624189748427652655102941025460326238168694857579440451283111900752703048105388636238067658728210614836152288944156940396556095730684424451732507757790626475790839367618298518731312847741549804680633927465036036579854814916390958269034498994208685838734728553904457922664987577356093436259903298968562438875139158936937832960385003877163146471220908310078215168637856163130372503106190164118392536797483029739735657830572903380017720291452050906375262810448584797102934741205694274618037452868617519789852780";
+    Map<Character, Integer> result = new HashMap<>();
+
+    for (char digit : data.toCharArray()) {
+      if (result.containsKey(digit)) {
+        result.put(digit, result.get(digit) + 1);
+      } else {
+        result.put(digit, 1);
+      }
+    }
+    System.out.println("HashMap of frequency: " + result); // get HashMap of frequency
+    Set<Character> resultKeys = result.keySet(); // get all keys from HashMap
+    Collection<Integer> resultValues = result.values(); // get all values from HashMap
+    List<Integer> orderedResultValues = resultValues.stream()
+        .collect(Collectors.toList()); // convert Collection to List
+    char[] orderedKeysAsc = Sorting.bubbleSort(resultKeys, orderedResultValues);
+    // -- not useful
+    Collections.sort(orderedResultValues, Collections.reverseOrder());
+    // -- end of not useful
+    System.out.println("\nresultKeys: " + resultKeys +
+        "\nresultValues: " + resultValues +
+        "\norderedResultValues: " + orderedResultValues +
+        "\norderedKeysAsc: " + orderedKeysAsc);
+  }
+}
+
+class Sorting {
+  public static char[] bubbleSort(Set<Character> keys, List<Integer> values) {
+    char[] orderedCharKeys = new char[values.size()];
+    int pos = 0;
+    for (Character c : keys) {
+      orderedCharKeys[pos++] = c;
+    }
+
+    Object[] orderedKeys = keys.toArray();
+    int n = values.size();
+    boolean swapped;
+    for (int i = 0; i < n - 1; i++) {
+      swapped = false;
+      for (int j = 0; j < n - 1 - i; j++) {
+        if (values.get(j) < values.get(j + 1)) {
+          int temp = values.get(j);
+          Object tempChar = orderedKeys[j];
+          char temporalChar = orderedCharKeys[j];
+          values.set(j, values.get(j + 1));
+          orderedKeys[j] = orderedKeys[j + 1];
+          orderedCharKeys[j] = orderedCharKeys[j + 1];
+          values.set(j + 1, temp);
+          orderedKeys[j + 1] = tempChar;
+          orderedCharKeys[j + 1] = temporalChar;
+          swapped = true;
+        }
+      }
+      if (!swapped) {
+        break;
+      }
+    }
+    System.out.println("values: " + values);
+    System.out.println("orderedKeys: " + orderedKeys);
+    System.out.print("orderedCharKeys: [");
+    for (int i = 0; i < orderedCharKeys.length; i++) {
+      System.out.print(orderedCharKeys[i]);
+      if (i < orderedCharKeys.length - 1) {
+        System.out.print(", ");
+      }
+    }
+    System.out.println("]");
+    return orderedCharKeys;
+  }
+}
+
+class ReadFile {
+  public static String returnContent(String filePath) {
+    String content = "";
+
+    try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+      while ((content = reader.readLine()) != null) {
+        System.out.println(content);
+        return content;
+      }
+    } catch (IOException e) {
+      System.err.println("Error reading file: " + e.getMessage());
+    }
+    return content;
+  }
+}
